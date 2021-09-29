a1597d315c595f350972c9835f63710d9eef75ab
Tyler Pooschke <tyler.pooschke@pm.me>
2021-09-28 23:08:51 -0500

    create add task and add project forms, no submission

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,24 @@
-const modal = document.querySelector(".add-task-modal");
-
-const btn = document.querySelector("button");
-
+const modal = document.querySelector(".add-modal");
+const addTaskButton = document.querySelector(".add-task")
 const span = document.querySelector("span");
+const addProjectButton =document.querySelector(".add-project")
+const modalContent = document.querySelector(".modal-content")
 
-btn.addEventListener("click", () => {
+addTaskButton.addEventListener("click", () => {
 	modal.style.display = "block";
+	addTask();
+});
+
+addProjectButton.addEventListener("click", () => {
+	modal.style.display = "block";
+	addProject();
 });
 
 span.addEventListener("click", () => {
 	modal.style.display = "none";
-});
-
-window.addEventListener("click", (event) => {
-	if (event.target == modal) modal.style.display = "none";
+	while (modalContent.hasChildNodes() && modalContent.lastChild !== span){
+		modalContent.removeChild(modalContent.lastChild)
+	}
 });
 
 const projects = document.querySelector(".fa-tasks");
@@ -45,10 +50,97 @@ function collapseSidebar() {
 		}
 	}
 }
-subTask.addEventListener("click", () => {
-	if (subTask.checked == true) {
-		subTaskText.style.textDecoration = "line-through";
-	} else {
-		subTaskText.style.textDecoration = "none";
-	}
+// subTask.addEventListener("click", () => {
+// 	if (subTask.checked == true) {
+// 		subTaskText.style.textDecoration = "line-through";
+// 	} else {
+// 		subTaskText.style.textDecoration = "none";
+// 	}
+// });
+
+
+const addTask = (() => {
+
+const form = document.createElement("form");
+	form.setAttribute("action", "#")
+	form.classList.add("task-form")
+
+const inputText = document.createElement("input")
+	inputText.setAttribute("type","text")
+	inputText.setAttribute("name","task-name")
+	inputText.setAttribute("id","task-name")
+	inputText.setAttribute("placeholder","Task Name")
+	inputText.setAttribute("autocomplete","off")
+	inputText.setAttribute("required", "true")
+	form.appendChild(inputText)
+
+const textarea = document.createElement("textarea")
+	textarea.setAttribute("name","task-description")
+	textarea.setAttribute("id","task-description")
+	textarea.setAttribute("row","4")
+	textarea.setAttribute("placeholder","Task Description")
+	form.appendChild(textarea)
+
+const br = document.createElement("br")
+	form.appendChild(br)
+
+const inputDate = document.createElement("input")
+	inputDate.setAttribute("type","date")
+	inputDate.setAttribute("name","due-date")
+	inputDate.setAttribute("id","due-date")
+	inputDate.setAttribute("required", "true")
+	form.appendChild(inputDate)
+
+const select = document.createElement("select")
+	select.setAttribute("name", "priority")
+	select.setAttribute("id","priority")
+	form.appendChild(select)
+
+const optionLow = document.createElement("option")
+	optionLow.setAttribute("value","low")
+	optionLow.innerText= "Low";
+	select.appendChild(optionLow)
+
+const optionMed = document.createElement("option")
+	optionLow.setAttribute("value","medium")
+	optionLow.innerText= "Medium";
+	select.appendChild(optionMed)
+
+const optionHigh = document.createElement("option")
+	optionLow.setAttribute("value","high")
+	optionLow.innerText= "High";
+	select.appendChild(optionHigh)
+
+const button = document.createElement("button")
+	button.setAttribute("type","button")
+	button.classList.add("center")
+	button.innerText="Submit"
+	form.appendChild(button)
+
+modalContent.appendChild(form);
+});
+
+
+const addProject = (() => {
+
+const form = document.createElement("form");
+	form.setAttribute("action", "#")
+	form.classList.add("project-form")
+	
+const inputText = document.createElement("input")
+	inputText.setAttribute("type","text")
+	inputText.setAttribute("name","project-name")
+	inputText.setAttribute("id","project-name")
+	inputText.setAttribute("placeholder","Project Name")
+	inputText.setAttribute("autocomplete","off")
+	inputText.setAttribute("required", "true")
+	form.appendChild(inputText)
+		
+const button = document.createElement("button")
+	button.setAttribute("type","button")
+	button.classList.add("center")
+	button.innerText="Submit"
+	form.appendChild(button)
+			
+modalContent.appendChild(form);
 });
