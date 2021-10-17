4634bcd8e0f0331ac99845e585ec290460a2144d
Tyler Pooschke <tyler.pooschke@pm.me>
2021-10-17 16:38:27 -0500

    Revert "added icons for task creation"
    This reverts commit cb22c5786a7df68c53f4f7e723ff522f9d3d83aa.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import { addProjectForm } from "./modules/project-form.js";
-import { addTaskForm, taskArray } from "./modules/task-form.js";
+import { addTaskForm } from "./modules/task-form.js";
 import { modal } from "./modules/modal.js";
 import { collapseSidebar } from "./modules/navbar-func.js";
-import { displayTask } from "./modules/task-func.js";
-import { taskArray } from "./modules/task-form.js";
 
 const addProjectButton = document.querySelector(".add-project");
 const content = document.querySelector(".content");
@@ -19,10 +17,6 @@ addTaskButton.addEventListener("click", () => {
 	modal.style.display = "block";
 	addTaskForm();
 });
-
-const defaultProject = document.querySelector("#default");
-
-defaultProject.addEventListener("click", displayTask(taskArray, "Default"));
 
 const projects = document.querySelector(".fa-tasks");
 const sidebar = document.querySelector(".side-bar");
